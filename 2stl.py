1e672610b78e133bc83413cc83c1de0ecd5419cd
Robert McWilliam <rmcw@allmail.net>
2018-07-28 15:30:20 +0100

    Make lower res version.

--- 2stl.py
+++ 2stl.py
@@ -4,7 +4,7 @@ from scipy.misc import imresize
 from scipy.ndimage import gaussian_filter
 
 A = 256 * imread("cantcatchme-2-fs.png")
-A = imresize(A, (512, 512))
+A = imresize(A, (256, 256))
 A = A[:, :, 2] + 1.0*A[:,:, 0] # Compose RGBA channels to give depth
 A = gaussian_filter(A, 1)  # smoothing
-numpy2stl(A, "gary.stl", scale=0.1, mask_val=5., solid=True)
+numpy2stl(A, "gary2.stl", scale=0.1, mask_val=5., solid=True)
